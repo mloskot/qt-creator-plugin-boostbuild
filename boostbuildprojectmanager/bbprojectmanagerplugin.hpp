646a30a9503432c47c81029af2ab640cb7f55591
Mateusz Loskot <mateusz@loskot.net>
2013-11-10 22:11:20 +0000

    Add comments with API use explanation

--- boostbuildprojectmanager/bbprojectmanagerplugin.hpp
+++ boostbuildprojectmanager/bbprojectmanagerplugin.hpp
@@ -8,6 +8,8 @@
 namespace BoostBuildProjectManager {
 namespace Internal {
 
+
+// Sole implementation of the IPlugin class for the extension.
 class BoostBuildPlugin : public ExtensionSystem::IPlugin
 {
     Q_OBJECT
@@ -17,12 +19,16 @@ public:
     BoostBuildPlugin();
     ~BoostBuildPlugin();
 
+    // Called as second step of the plugins loading.
+    // The initialize functions are called in root-to-leaf order of the dependency tree.
     bool initialize(QStringList const& arguments, QString* errorString);
+
+    // Called as final step of the plugins loading.
+    // At this point, all dependencies along the plugin dependency tree
+    // have been initialized completely.
     void extensionsInitialized();
+
     ShutdownFlag aboutToShutdown();
-
-private slots:
-
 };
 
 } // namespace Internal
